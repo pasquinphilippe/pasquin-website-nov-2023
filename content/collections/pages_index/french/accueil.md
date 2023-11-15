311f4259667190a709d286f6ea926d392ea6f834
Philippe Buzztroop <philippe@milistack.com>
2023-11-15 14:45:10 -0500

    Good polishing

--- content/collections/pages_index/french/accueil.md
+++ content/collections/pages_index/french/accueil.md
@@ -4,11 +4,58 @@ blueprint: homepage
 title: Accueil
 seo_title: 'Développeur Shopify Montréal | Philippe Pasquin'
 updated_by: cd3700e4-11c6-42b6-8dbb-6134449b1279
-updated_at: 1700014586
+updated_at: 1700073418
 og_image: accueil-og-1700014133.png
 seo_noindex: false
 seo_nofollow: false
 seo_canonical_type: entry
 sitemap_change_frequency: weekly
 sitemap_priority: '1'
+announcement: true
+related_date: '2023-12-01'
+announcement_text: 'Nouveau prix pour notre support mensuel'
+heading: 'Construisez, évoluez, excellez avec notre expertise Shopify'
+paragraph: 'En tant que partenaire Shopify et Shopify Plus, nous développons, optimisons et assurons la maintenance de solutions eCommerce sur Shopify.'
+button_link: 'entry::d9172796-78a7-4e6c-b716-53ba1946125e'
+button_text: 'Planifier une rencontre découverte'
+subheading: 'Support mensuel sur Shopify'
+support_heading: 'Déléguez vos tâches de développement Shopify.'
+support_pragraph:
+  -
+    type: paragraph
+    content:
+      -
+        type: text
+        text: 'Choisissez un plan qui fonctionne pour vous'
+ctas:
+  -
+    id: lp03aree
+    icon: philippe_pasquin.png
+    heading: 'Vous avez des questions?'
+    pragraph: 'Organiser un appel découverte avec moi afin de vous présentez nos abonnements'
+    link_text: 'Réservez une démo'
+    modal: false
+    link: 'entry::d9172796-78a7-4e6c-b716-53ba1946125e'
+    type: cta
+    enabled: true
+  -
+    id: lp03c8mw
+    icon: refer.svg
+    heading: 'Référez moi un client et recevez 200$'
+    pragraph: 'Recevez 200$ à la signature et 8% de commissions récurrentes.'
+    link_text: 'Référez un client'
+    modal: true
+    modal_alpine_name: modalRefer
+    type: cta
+    enabled: true
+services_heading: 'Nos services connexes'
+availability: 'Disponible dans le prochain mois'
+contact_heading: 'Vous avez une question?'
+contact_cta_text_1: 'Prenons contact'
+contact_cta_link_1: 'entry::d9172796-78a7-4e6c-b716-53ba1946125e'
+contact_cta_text_2: 'Planifier une démonstration'
+contact_cta_link_2: 'entry::d9172796-78a7-4e6c-b716-53ba1946125e'
+projects_heading: 'Mes derniers projets'
+btn_cacher: 'Masquer les projets'
+btn_plus: 'Voir plus de projets'
 ---
--- resources/js/site.js
+++ resources/js/site.js
@@ -1,71 +1,73 @@
-import Alpine from 'alpinejs'
-import collapse from '@alpinejs/collapse'
-import morph from '@alpinejs/morph'
-import persist from '@alpinejs/persist'
-import focus from '@alpinejs/focus'
+import Alpine from "alpinejs";
+import collapse from "@alpinejs/collapse";
+import morph from "@alpinejs/morph";
+import persist from "@alpinejs/persist";
+import focus from "@alpinejs/focus";
 import "font-awesome/css/font-awesome.min.css";
-
 
 // Global get CSRF token function (used by forms).
 window.getToken = async () => {
-    return await fetch('/!/statamic-peak-tools/dynamic-token/refresh')
+    return await fetch("/!/statamic-peak-tools/dynamic-token/refresh")
         .then((res) => res.json())
         .then((data) => {
-            return data.csrf_token
+            return data.csrf_token;
         })
         .catch((error) => {
-            this.error = 'Something went wrong. Please try again later.'
-        })
-}
+            this.error = "Something went wrong. Please try again later.";
+        });
+};
 
-document.addEventListener("DOMContentLoaded", function() {
-  const magneticElements = document.querySelectorAll(".magnetic");
+document.addEventListener("DOMContentLoaded", function () {
+    const magneticElements = document.querySelectorAll(".magnetic");
 
-  magneticElements.forEach(element => {
-      element.addEventListener("mousemove", function(event) {
-          if (window.innerWidth > 540) {
-              const rect = element.getBoundingClientRect();
-              const strength = element.getAttribute("data-strength") || 20; // Default strength if not set
-              const x = ((event.clientX - rect.left) / element.offsetWidth - 0.5) * strength;
-              const y = ((event.clientY - rect.top) / element.offsetHeight - 0.5) * strength;
+    magneticElements.forEach((element) => {
+        element.addEventListener("mousemove", function (event) {
+            if (window.innerWidth > 540) {
+                const rect = element.getBoundingClientRect();
+                const strength = element.getAttribute("data-strength") || 20; // Default strength if not set
+                const x =
+                    ((event.clientX - rect.left) / element.offsetWidth - 0.5) *
+                    strength;
+                const y =
+                    ((event.clientY - rect.top) / element.offsetHeight - 0.5) *
+                    strength;
 
-              element.style.transform = `translate(${x}px, ${y}px)`;
-          }
-      });
+                element.style.transform = `translate(${x}px, ${y}px)`;
+            }
+        });
 
-      element.addEventListener("mouseleave", function() {
-          element.style.transform = `translate(0px, 0px)`;
-      });
-  });
+        element.addEventListener("mouseleave", function () {
+            element.style.transform = `translate(0px, 0px)`;
+        });
+    });
 });
 
 // Call Alpine.
-window.Alpine = Alpine
-Alpine.plugin([collapse, focus, morph, persist])
+window.Alpine = Alpine;
+Alpine.plugin([collapse, focus, morph, persist]);
 // Define your marquee component here
 window.marquee = function () {
     return {
-      init() {
-        this.startMarquee();
-      },
-      startMarquee() {
-        const marqueeElement = this.$refs.marquee;
-        let marqueeWidth = marqueeElement.scrollWidth / 2; // Assume the content is duplicated, so we use half the width
-        let x = 0;
-  
-        const marquee = () => {
-          x -= 0.6; // Adjust speed by changing the value here
-          if (Math.abs(x) >= marqueeWidth) {
-            x = 0; // Reset x to 0 instead of marqueeWidth to loop back to the start
-          }
-          marqueeElement.style.transform = `translateX(${x}px)`;
-        };
-  
-        // Start the marquee
-        setInterval(marquee, 10); // Adjust interval for speed
-      }
+        init() {
+            this.startMarquee();
+        },
+        startMarquee() {
+            const marqueeElement = this.$refs.marquee;
+            let marqueeWidth = marqueeElement.scrollWidth / 2; // Assume the content is duplicated, so we use half the width
+            let x = 0;
+
+            const marquee = () => {
+                x -= 0.6; // Adjust speed by changing the value here
+                if (Math.abs(x) >= marqueeWidth) {
+                    x = 0; // Reset x to 0 instead of marqueeWidth to loop back to the start
+                }
+                marqueeElement.style.transform = `translateX(${x}px)`;
+            };
+
+            // Start the marquee
+            setInterval(marquee, 10); // Adjust interval for speed
+        },
     };
-  };
-  
-  Alpine.start();
-  
+};
+
+Alpine.start();
