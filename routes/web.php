f20e924c6681431196b9ba5e812ce837bd5e97ad
Philippe Buzztroop <philippe@milistack.com>
2023-11-27 16:25:30 -0500

    rock hard

--- routes/web.php
+++ routes/web.php
@@ -8,26 +8,24 @@ use Statamic\Facades\URL;
 $useSitemap = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_sitemap');
 $useSocialImageGeneration = (bool)GlobalSet::findByHandle('seo')?->inDefaultSite()?->get('use_social_image_generation');
 
-// The Sitemap Index route for listing sitemaps of all (multi)sites.
-if ($useSitemap) {
+// The Sitemap Index route for listing sitemaps of all (multi)sites.if ($useSitemap) {
     Route::statamic('/sitemaps.xml', 'statamic-peak-seo::sitemap/sitemaps', [
         'layout' => null,
         'content_type' => 'application/xml'
     ]);
-}
 
 // Register routes for each site.
-if ($useSitemap || $useSocialImageGeneration) {
+
     Site::all()->each(function (\Statamic\Sites\Site $site) use ($useSitemap, $useSocialImageGeneration) {
         $relativeSiteUrl = URL::makeRelative($site->url());
 
         // The Sitemap route.
-        if ($useSitemap) {
+   
             Route::statamic(URL::tidy($relativeSiteUrl . '/sitemap.xml'), 'statamic-peak-seo::sitemap/sitemap', [
                 'layout' => null,
                 'content_type' => 'application/xml'
             ]);
-        }
+        
 
         // The Social Image route to generate social images.
         if ($useSocialImageGeneration) {
@@ -36,4 +34,3 @@ if ($useSitemap || $useSocialImageGeneration) {
             ]);
         }
     });
-}
